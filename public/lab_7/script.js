b4b8eaa0271c95a751f3429041175033c047d2b1
Colin Bradley <colinbradley68@gmail.com>
2020-10-29 01:41:13 -0400

    Update script.js

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -14,9 +14,8 @@ function convertRestaurantsToCategories(restaurantList) {
     result[catArray[i]] += 1;
   }
   const mapArr = Object.keys(result).map((category) => {
-    newArr = {}
-    newArr.y = result[category];
-    newArr.label = category;
+    y : result[category];
+    label : category;
 
   });
   console.log(result);
