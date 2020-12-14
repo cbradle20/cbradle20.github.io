8ce7fd114cd0ecbf3fdce7d6a4e0abc9844fe660
Colin Bradley <colinbradley68@gmail.com>
2020-12-14 11:13:00 -0500

    Update script.js

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -36,27 +36,8 @@ document.body.addEventListener('submit', async (e) => {
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
       // You're going to do your lab work in here. Replace this comment.
-      if(document.querySelector('.flex-inner')){
-        document.querySelector('.flex-inner').remove();
-      }
-      const ol = document.createElement('ol');
-      ol.className = 'flex-inner';
-      $('form').prepend(ol);
+      
 
-      const newArr = range(10);
-      const newArr2 = newArr.map(() => {
-        const number = getRandomIntInclusive(0, 243);
-        return fromServer[number];
-      });
-
-      const reverseList = newArr2.sort((org, compare) => sortByKey(org, compare, 'name'));
-
-      reverseList.forEach((el, i) => {
-        const li = document.createElement('li');
-        $(li).append(`<input type="checkbox" value=${el.code} id=${el.code} />`);
-        $(li).append(`<label for=${el.code}>${el.name}</label>`);
-        $(ol).append(li);
-      });
     })
     .catch((err) => console.log(err));
 });
